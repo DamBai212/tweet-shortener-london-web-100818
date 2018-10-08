408a877d0b03943a544f0b642bc23910414bb68c
DamBai212 <damanybailey@icloud.com>
2018-10-08 02:48:11 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,8 @@
-# Write your code here.
+def shortened_tweet_truncator(tweet_three)
+  if tweet_three.length > 140
+    word_substituter(tweet_three)
+     tweet_three[0..136] + "..."
+  else
+    tweet_three
+  end
+end
